218a3a764ba5cdc15281203e2ab15533e7c8923d
peppacatt <peppacatt@163.com>
2020-09-23 20:58:32 +0800

    create OutOfMemory

---
+++ springboot_outofmemory/src/main/java/com/cy/SpringbootOutofmemoryApplication.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOutofmemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOutofmemoryApplication.class, args);
+    }
+
+}
